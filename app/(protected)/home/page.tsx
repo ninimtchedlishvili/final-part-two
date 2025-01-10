d62f9e937e792cadeb1673fe8992f38e2b2509fa
Nini Mtchedlishvili <145496919+mtchedlishvilinini@users.noreply.github.com>
2025-01-11 01:11:21 +0400

    fixed bug on movie page

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -2,21 +2,10 @@ import Footer from "@/app/(protected)/components/layout/Footer";
 import Page from "../movie/Page";
 
 export default async function Home() {
-  let movies = [];
-  try {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1",
-      { next: { revalidate: 10 } }
-    );
-    const response = await data.json();
-    movies = response || [];
-  } catch (error) {
-    console.error("Failed to fetch movies:", error);
-  }
 
   return (
     <div className="bg-gray-900">
-      <Page movies={movies} />
+      <Page  />
       <Footer />
     </div>
   );
